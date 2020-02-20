38a8c8f810f7c086148bc11356703529ee1960b8
jakaria08 <jakaria.rabbi@live.com>
2020-02-19 19:32:48 -0700

    eleven point precision

--- lib/Evaluator.py
+++ lib/Evaluator.py
@@ -286,8 +286,8 @@ class Evaluator:
                 plt.show()
                 # plt.waitforbuttonpress()
                 plt.pause(0.05)
-        print(type(result['precision'])
-        print(type(result['recall'])
+        print(type(result['precision']))
+        print(type(result['recall']))
         return results
 
     @staticmethod
